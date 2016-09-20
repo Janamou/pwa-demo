7987a79b6b85fd2c9fc81531e45b836e2a49a3d4
Jana Moudrá <jana.moudra@gmail.com>
2016-09-21 01:10:01 +0200

    The app source code

--- app/js/main.js
+++ app/js/main.js
@@ -1,102 +1,114 @@
-/*!
- *
- *  Web Starter Kit
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-/* eslint-env browser */
 (function () {
-    'use strict';
+    "use strict";
 
     // Check to make sure service workers are supported in the current browser,
-    // and that the current page is accessed from a secure origin. Using a
-    // service worker from an insecure origin will trigger JS console errors. See
-    // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
-    var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+    // and that the current page is accessed from a secure origin.
+    var isLocalhost = Boolean(window.location.hostname === "localhost" ||
         // [::1] is the IPv6 localhost address.
-        window.location.hostname === '[::1]' ||
+        window.location.hostname === "[::1]" ||
         // 127.0.0.1/8 is considered localhost for IPv4.
         window.location.hostname.match(
             /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
         )
     );
 
-    if ('serviceWorker' in navigator &&
-        (window.location.protocol === 'https:' || isLocalhost)) {
-        navigator.serviceWorker.register('./service-worker.js')
-            .then(function (registration) {
-                // updatefound is fired if service-worker.js changes.
-                registration.onupdatefound = function () {
-                    // updatefound is also fired the very first time the SW is installed,
-                    // and there's no need to prompt for a reload at that point.
-                    // So check here to see if the page is already controlled,
-                    // i.e. whether there's an existing service worker.
-                    if (navigator.serviceWorker.controller) {
-                        // The updatefound event implies that registration.installing is set:
-                        // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-                        var installingWorker = registration.installing;
-
-                        installingWorker.onstatechange = function () {
-                            switch (installingWorker.state) {
-                                case 'installed':
-                                    // At this point, the old content will have been purged and the
-                                    // fresh content will have been added to the cache.
-                                    // It's the perfect time to display a "New content is
-                                    // available; please refresh." message in the page's interface.
-                                    break;
-
-                                case 'redundant':
-                                    throw new Error('The installing ' +
-                                        'service worker became redundant.');
-
-                                default:
-                                // Ignore
-                            }
-                        };
-                    }
-                };
+    if ("serviceWorker" in navigator &&
+        (window.location.protocol === "https:" || isLocalhost)) {
+        navigator.serviceWorker.register("./service-worker.js")
+            .then(function (register) {
+                //TODO something to do here
             }).catch(function (e) {
-            console.error('Error during service worker registration:', e);
+            console.error("Error during service worker registration:", e);
         });
     }
 
-    // Your custom JavaScript goes here
+    function App() {
+        this.container = document.getElementById("content");
+        this.cardTemplate = document.getElementById("card-template");
+        this.refresh = document.getElementById("refresh");
+        this.refresh.addEventListener("click", e => {
+            this.clearAll();
+            this.loadData();
+        });
 
-    setTimeout(function() {
+        this.spinner = document.querySelector(".mdl-spinner");
+    }
+
+    App.prototype.init = function () {
+        this.loadData();
+    };
+
+    App.prototype.loadData = function () {
+        //setTimeout(function() {
         var url = "https://api.github.com/search/repositories?q=pwa";
-        if ("caches" in window) {
-            /*
-             * Check if the service worker has already cached this city's weather
-             * data. If the service worker has the data, then display the cached
-             * data while the app fetches the latest data.
-             */
-            caches.match(url).then(function (response) {
-                if (response) {
-                    response.json().then(function updateFromCache(json) {
-                        console.log(json);
-                    });
-                }
+        this.spinner.classList.add("is-active");
+
+        var request = new Request(url);
+        fetch(request).then(response => {
+            response.json().then(json => {
+                this.showData(json);
             });
+        }).catch(e => {
+            if ("caches" in window) {
+                caches.match(url).then(response => {
+                    if (response) {
+                        response.json().then(json => {
+                            this.showData(json);
+                        });
+                    }
+                });
+            }
+        });
+        //}, 1000);
+    };
+
+    App.prototype.showData = function (data) {
+        var items = data.items;
+
+        for (var i = 0; i < items.length; i++) {
+            this.createCard(items[i]);
+        }
+        this.spinner.classList.remove("is-active");
+    };
+
+    App.prototype.createCard = function (item) {
+        var card = this.cardTemplate.cloneNode(true);
+        card.style.display = "block";
+
+        var cardContent = card.querySelector(".mdl-card__supporting-text");
+        cardContent.querySelector("h4").textContent = item.name;
+
+        var textDescription = item.description;
+        var cardDescription = cardContent.querySelector("p");
+        if (textDescription) {
+            cardDescription.innerText = item.description;
+        } else {
+            cardDescription.innerText = "No description available";
+            cardDescription.classList.add("not-available");
         }
 
-        var req = new Request(url);
-        fetch(req).then(function (res) {
-            return res.json().then(function (json) {
-                console.log(json);
-            });
-        });
-    }, 1000);
+        var cardLink = card.querySelector(".mdl-card__actions");
+        cardLink.querySelector("a").href = item.html_url;
+
+        var cardStars = card.querySelector(".mdl-card__metadata-stars span");
+        cardStars.textContent = item.stargazers_count;
+        var cardWatchers = card.querySelector(".mdl-card__metadata-watchers span");
+        cardWatchers.textContent = item.watchers_count;
+        var cardForks = card.querySelector(".mdl-card__metadata-forks span");
+        cardForks.textContent = item.forks_count;
+
+        this.container.appendChild(card);
+    };
+
+    App.prototype.clearAll = function () {
+        while (this.container.firstChild) {
+            this.container.removeChild(this.container.firstChild);
+        }
+    };
+
+    window.onload = function () {
+        var app = new App();
+        app.init();
+    };
 })
 ();
