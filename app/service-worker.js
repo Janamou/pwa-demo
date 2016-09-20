9feac3cbddd84cbf19c559e0bc66ab14ee421acc
Jana Moudrá <jana.moudra@gmail.com>
2016-09-20 15:14:58 +0200

    Service worker update

--- app/service-worker.js
+++ app/service-worker.js
@@ -18,7 +18,7 @@ self.addEventListener("install", function (e) {
     console.log("ServiceWorker installed.");
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
-            console.log("Adding all the App shell files to cache.");
+            console.log("ServiceWorker adding all the App shell files to cache.");
             return cache.addAll(files);
         })
     );
@@ -31,7 +31,7 @@ self.addEventListener("activate", function (e) {
         caches.keys().then(function (keyList) {
             return Promise.all(keyList.map(function (key) {
                 if (key !== cacheName && key !== contentCacheName) {
-                    console.log("Removing old data from the cache.", key);
+                    console.log("ServiceWorker removing old data from the cache.", key);
                     return caches.delete(key);
                 }
             }));
@@ -44,13 +44,15 @@ var contentUrl = "https://api.github.com/search/repositories?q=pwa";
 
 // Fetch event. Searches for the App shell data in the cache and retrieves
 // them from the cache if available.
+// It also caches the actual request to github search and uses it in case
+// of connectivity issues.
 self.addEventListener("fetch", function (e) {
     console.log("ServiceWorker fetch.");
     if (e.request.url.indexOf(contentUrl) >= 0) {
         e.respondWith(
             caches.open(contentCacheName).then(function (cache) {
                 return fetch(e.request).then(function (response) {
-                    console.log("Caching response.");
+                    console.log("ServiceWorker caching response.");
                     cache.put(e.request.url, response.clone());
                     return response;
                 });
